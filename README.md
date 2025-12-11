17ad4daa13ebf606db7179d7f8d4318c82b32edd
Chirag R <chirag.r@tetherfi.com>
2025-12-11 08:36:32 +0530

    initial commit - producer logic

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+## Python create new virtual environment
+```shell    
+    python -m venv venv
+    venv\Scripts\activate.bat
+    .\venv\Scripts\Activate.ps1
+
+```
---
+++ producer.py
@@ -0,0 +1,34 @@
+from confluent_kafka import Producer
+import uuid
+import json
+producer_config = {
+    "bootstrap.servers": "localhost:9092"
+    }
+
+Producer = Producer(producer_config)
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"❌ Delivery failed for Order {msg.key()}: {err}")
+    else:
+        print(f"✅ Order delivered {msg.value().decode("utf-8")} successfully")
+        print(f"✅ Delivered to {msg.topic()}, partition: {msg.partition()}, at offset: {msg.offset()}")
+        print(dir(msg))
+
+order = {
+    "order_id": str(uuid.uuid4()),  
+    "user": "name",
+    "item": "mushroom pizza",
+    "quantity": 2,
+}
+
+value = json.dumps(order).encode("utf-8")
+
+Producer.produce(
+    topic="orders",
+    value=value,
+    callback=delivery_report
+)
+
+Producer.flush()
