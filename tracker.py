17c1868b8802055632ddef6d1e448ce7a68c9c06
chriag-ramdas <chiragmangalore@gmail.com>
2025-12-13 03:26:24 +0530

    update

--- tracker.py
+++ tracker.py
@@ -2,7 +2,7 @@ import json
 from confluent_kafka import Consumer
 
 consumer_config = {
-    "bootstrap.servers": "localhost:9092",  
+    "bootstrap.servers": "localhost:9092",
     "group.id": "order-tracker",
     "auto.offset.reset": "earliest"
 }
@@ -15,13 +15,13 @@ print("🚚 Order Tracker is running and listening for orders...")
 try:
     while True:
         msg = consumer.poll(1.0)  # Timeout of 1 second
-
+        print(f"Polled message: {msg}")
         if msg is None:
             continue
         if msg.error():
             print(f"❌ Consumer error: {msg.error()}")
             continue
-        
+
         value = msg.value().decode("utf-8")
         order = json.loads(value)
         print(f"📦 New order received: {order['quantity']} x {order['item']}")
